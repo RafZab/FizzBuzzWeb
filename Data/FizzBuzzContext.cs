37a38257762c3e9d5eee51917bff2c26fdb6c6ff
Rafał Zaborowski <raffik2000@gmail.com>
2021-04-07 21:36:44 +0200

    Add Datebase

---
+++ Data/FizzBuzzContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FizzBuzz.Models;
+
+
+namespace FizzBuzz.Data
+{
+    public class FizzBuzzContext : DbContext
+    {
+        public DbSet<Fizzbuzz> Fizzbuzz { get; set; }
+        public FizzBuzzContext(DbContextOptions options) : base(options) { }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -4,10 +4,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FizzBuzz.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace FizzBuzz
 {
@@ -23,6 +21,10 @@ namespace FizzBuzz
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<FizzBuzzContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("EFDemoDB"));
+            });
+
             services.AddRazorPages();
 
             services.AddMemoryCache();
