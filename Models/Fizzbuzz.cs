af3a8e9c5f5dd31b2cef78f15bc7d8b8d77401c7
Rafał Zaborowski <raffik2000@gmail.com>
2021-04-07 21:36:44 +0200

    Add Datebase

---
+++ Models/Fizzbuzz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Models
+{
+    public class Fizzbuzz
+    {
+        public int Id { get; set; }
+        [Required]
+        [Range(1, 1000)]
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Column(TypeName = "varchar(50)")]
+        public string Result { get; set; }
+    }
+}
